c9c28769131c74bab1d941d048824bc5de9631a3
Rodr1go <rodrigosc.pi@gmail.com>
2019-07-01 08:54:24 -0300

    Adicionando passport ao projeto

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -90,20 +90,29 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="fa fa-circle-o nav-icon"></i>
-                  <p>Active Page</p>
-                </a>
+                <router-link to="/users" class="nav-link">
+                  <i class="fa fa-users nav-icon"></i>
+                  <p>Usuários</p>
+                </router-link>
               </li>
-              <li class="nav-item">
+              <!-- <li class="nav-item">
                 <a href="#" class="nav-link">
                   <i class="fa fa-circle-o nav-icon"></i>
                   <p>Inactive Page</p>
                 </a>
-              </li>
+              </li> -->
             </ul>
           </li>
           
+          <li class="nav-item">
+            <router-link to="/desenvolvedor" class="nav-link">
+              <i class="nav-icon fas fa-cogs"></i>
+              <p>
+                Desenvolvedor
+              </p>
+            </router-link>
+          </li>
+
           <li class="nav-item">
             <router-link to="/perfil" class="nav-link">
               <i class="nav-icon fas fa-user orange"></i>
