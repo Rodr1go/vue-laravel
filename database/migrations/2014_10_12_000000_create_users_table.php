0c3722143a68358474204922df34fd8b00bf44c6
Rodr1go <rodrigosc.pi@gmail.com>
2019-07-25 12:31:41 -0300

    corrige migration de usuario

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,9 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->String('type')->default('user');
+            $table->string('type')->default('user');
             $table->mediumText('bio')->nullable();
-            $table->String('photo')->default('profile.png');
+            $table->string('photo')->default('profile.png');
             $table->rememberToken();
             $table->timestamps();
         });
